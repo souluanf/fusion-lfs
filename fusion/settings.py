a3636d03da9b8a8536b5cebecb719ccb47ad9fa1
Luan Fernandes <souluan@live.com>
2020-06-29 02:17:57 -0300

    update settings

--- fusion/settings.py
+++ fusion/settings.py
@@ -14,6 +14,7 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', 'development')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
